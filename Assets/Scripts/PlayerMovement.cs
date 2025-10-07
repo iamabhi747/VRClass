7313f2c19815d5aeb9d68c107884ad8eebac0086
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2025-10-07 16:22:17 +0530

    Head Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,16 @@ using System.Collections.Generic;
 public class PlayerMovement : MonoBehaviour
 {
     // Variables
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float walkSpeed;
-    [SerializeField] private float runSpeed;
-    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private float moveSpeed = 0f;
+    [SerializeField] private float walkSpeed = 2f;
+    [SerializeField] private float runSpeed = 5f;
+    [SerializeField] private float mouseSensitivity = 250f;
+    [SerializeField] private float verticalLookLimit = 30f;
+    [SerializeField] private float horizontalLookLimit = 60f;
 
+    private float verticalRotation = 0f;
+    private float horizontalHeadRotation = 0f;
+    private Vector3 lookAtPosition;
     private Vector3 moveDirection;
     private Vector3 velocity;
 
@@ -22,6 +27,8 @@ public class PlayerMovement : MonoBehaviour
     // References
     private CharacterController characterController;
     private Animator animator;
+    private Camera playerCamera;
+    private Transform headBone;
 
     // Methods
 
@@ -29,6 +36,17 @@ public class PlayerMovement : MonoBehaviour
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponentInChildren<Animator>();
+        playerCamera = GetComponentInChildren<Camera>();
+
+        headBone = animator.GetBoneTransform(HumanBodyBones.Head);
+        // Debug.Log("Head Bone: " + headBone.name);
+
+        playerCamera.transform.position = headBone.position;
+        playerCamera.transform.SetParent(headBone);
+
+        GameObject animatorObject = animator.gameObject;
+        var ikProxy = animatorObject.AddComponent<IKProxy>();
+        ikProxy.playerMovementScript = this;
 
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -37,6 +55,12 @@ public class PlayerMovement : MonoBehaviour
     {
         Move();
         Rotate();
+    }
+
+    private void ExecuteAnimatorIK()
+    {
+        animator.SetLookAtWeight(1.0f);
+        animator.SetLookAtPosition(lookAtPosition);
     }
 
     private void Move()
@@ -76,8 +100,25 @@ public class PlayerMovement : MonoBehaviour
                 animator.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
             }
 
-            moveDirection = transform.TransformDirection(moveDirection);
+            float totalHorizontalRotation = transform.eulerAngles.y + horizontalHeadRotation;
+            Vector3 headForward = new Vector3(
+                Mathf.Sin(totalHorizontalRotation * Mathf.Deg2Rad),
+                0,
+                Mathf.Cos(totalHorizontalRotation * Mathf.Deg2Rad)
+            );
+
+            moveDirection = headForward * moveDirection.z;
             moveDirection *= moveSpeed;
+
+            // Auto-align body to head direction when moving (smooth body rotation)
+            if (moveDirection.magnitude > 0.1f && Mathf.Abs(horizontalHeadRotation) > 5f)
+            {
+                float bodyRotationAmount = horizontalHeadRotation * Time.deltaTime * 4f;
+                transform.Rotate(0, bodyRotationAmount, 0);
+                horizontalHeadRotation -= bodyRotationAmount;
+                // Clamp after modification
+                horizontalHeadRotation = Mathf.Clamp(horizontalHeadRotation, -horizontalLookLimit, horizontalLookLimit);
+            }
 
             // if (Input.GetKeyDown(KeyCode.Space))
             // {
@@ -95,7 +136,32 @@ public class PlayerMovement : MonoBehaviour
     public void Rotate()
     {
         float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
 
-        transform.Rotate(Vector3.up * mouseX * mouseSensitivity * Time.deltaTime);
+        horizontalHeadRotation += mouseX * mouseSensitivity * Time.deltaTime;
+        horizontalHeadRotation = Mathf.Clamp(horizontalHeadRotation, -horizontalLookLimit, horizontalLookLimit);
+
+        verticalRotation -= mouseY * mouseSensitivity * Time.deltaTime;
+        verticalRotation = Mathf.Clamp(verticalRotation, -verticalLookLimit, verticalLookLimit);
+
+        Quaternion headRotation = Quaternion.Euler(verticalRotation, transform.eulerAngles.y + horizontalHeadRotation, 0);
+        Vector3 direction = headRotation * Vector3.forward;
+
+        lookAtPosition = headBone.position + direction * 15f;
     }
+
+    private class IKProxy : MonoBehaviour
+    {
+        public PlayerMovement playerMovementScript;
+
+        void OnAnimatorIK(int layerIndex)
+        {
+            // Just forward the call to the main script.
+            if (playerMovementScript != null)
+            {
+                playerMovementScript.ExecuteAnimatorIK();
+            }
+        }
+    }
+
 }
